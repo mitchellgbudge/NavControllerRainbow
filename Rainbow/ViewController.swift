49d2e272e6f5d59105da1fcc2cd8ef8697f64cf1
Mitchell Budge <mitchellbudge@gmail.com>
2019-08-12 10:13:06 -0600

    Add base Xcode project

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Mitchell Budge on 8/12/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
