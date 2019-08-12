d351b019951a51b7ebffda31585e0542ca1a3b33
Mitchell Budge <mitchellbudge@gmail.com>
2019-08-12 10:47:34 -0600

    Configure table view data source methods

---
+++ Rainbow/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Mitchell Budge on 8/12/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
